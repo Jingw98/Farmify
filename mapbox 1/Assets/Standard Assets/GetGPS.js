0e069b55eba10203419777e2a71a5d96df00e401
Syrend <jwan501@aucklanduni.ac.nz>
2017-10-21 08:26:16 +1300

    gps vector

--- mapbox 1/Assets/Standard Assets/GetGPS.js
+++ mapbox 1/Assets/Standard Assets/GetGPS.js
@@ -1,11 +1,14 @@
 ﻿#pragma strict
-
+import UnityEngine.UI;
 var tim;
-var lat;
-var lon;
-var locStyle : GUIStyle;
+static  var lat;
+static  var lon;
+ var latField : UI.Text;
+ var lonField : UI.Text;
+ var locStyle : GUIStyle;
 
 function Start () {
+ 
 	// First, check if user has location service enabled
 	if (!Input.location.isEnabledByUser)
 		return;
@@ -18,32 +21,40 @@ function Start () {
 	while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) {
 		yield WaitForSeconds (1);
 		maxWait--;
+	
 	}
 
 	// Service didn't initialize in 20 seconds
 	if (maxWait < 1) {
+
 		print ("Timed out");
 		return;
 	}
 
 	// Connection has failed
 	if (Input.location.status == LocationServiceStatus.Failed) {
+
 		print ("Unable to determine device location");
 		return;
 	}
-
+	lat = -36.850112;
+	lon = 174.761751;
 }
 
 function Update () {
 	// Access granted and location value could be retrieved
-	lat = Input.location.lastData.latitude;
-	lon = Input.location.lastData.longitude;
-	tim = Input.location.lastData.timestamp;
-	
+	//lat = Input.location.lastData.latitude;
+	//lon = Input.location.lastData.longitude;
+    //tim = Input.location.lastData.timestamp;
+    lat = -37.850112;
+    lon = 175.761751;
+	latField.text=""+lat;
+	lonField.text=""+lon;
 }
 
-function OnGUI () {
-	GUI.Box (Rect(Screen.width - 205, 100, 200, 40), "Time: " + tim, locStyle);
-	GUI.Box (Rect(Screen.width - 205, 140, 200, 40), "Lat: " + lat, locStyle);
-	GUI.Box (Rect(Screen.width - 205, 180, 200, 40), "Lon: " + lon, locStyle);
-}
+//function OnGUI () {
+//	GUI.Box (Rect(Screen.width - 205, 100, 200, 40), "Time: " + tim, locStyle);
+//	GUI.Box (Rect(Screen.width - 205, 140, 200, 40), "Lat: " + lat, locStyle);
+//	GUI.Box (Rect(Screen.width - 205, 180, 200, 40), "Lon: " + lon, locStyle);
+	
+//}
